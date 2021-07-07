38c0e3694affe613970ffebffce4cf1c828189eb
Umang Galaiya <umanghome@gmail.com>
2021-07-07 12:52:55 +0530

    Use Card

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,47 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+import { onMount } from "svelte";
+import Card from "./components/Card.svelte";
+
+import { getCharacters } from "./lib/characters";
+
+let characters = [];
+let index = 0;
+
+$: character = characters[index];
+
+onMount(async () => {
+  characters = await getCharacters();
+});
+
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  {#if character}
+    <Card {character} />
+  {/if}
 </main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  :global(*) {
+    box-sizing: border-box;
   }
 
+  :global(html),
+  :global(body),
+  :global(#app) {
+    height: 100%;
+    width: 100%;
+    padding: 0;
+    margin: 0;
+  }
   main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
+    height: 100%;
+    width: 100%;
+    
+    padding: 2em;
 
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
   }
 </style>
